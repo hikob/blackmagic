e843f70339d527b5d86cd0f6f104bfdbd39e78d9
Clement Burin des Roziers <clement.burin-des-roziers@hikob.com>
2014-04-25 11:52:47 +0200

    EFM32 power consumption: added double buffering for USB

--- src/platforms/efm32/usbpowercon.c
+++ src/platforms/efm32/usbpowercon.c
@@ -19,6 +19,7 @@
 
 #include "platform.h"
 #include "dmactrl.h"
+#include "usbconfig.h"
 
 #include "usbpowercon.h"
 #include "jaguar.h"
@@ -26,11 +27,36 @@
 static void powercon_measure(uint32_t rtc_timestamp, float voltage, float shunt,
         float current);
 
+static int UsbDataTransmitted(USB_Status_TypeDef status, uint32_t xferred,
+        uint32_t remaining);
+static void send_current_buffer();
+static float get_timestamp(uint32_t rtc_timestamp);
+
 cdcLineCoding_TypeDef usbpowercon_cdclinecoding;
 
 static volatile int activated = 0;
-static volatile int locked = 0;
-static char power_buffer[256];
+
+enum
+{
+    BUF_MAX_SIZE = 64
+};
+
+/** Double buffered write */
+static char power_buffer_0[BUF_MAX_SIZE];
+static char power_buffer_1[BUF_MAX_SIZE];
+
+static const char* power_buffers[2] =
+{ power_buffer_0, power_buffer_1 };
+
+/** Double buffered sizes */
+static int power_buffer_sizes[2] =
+{ 0, 0 };
+
+/** Double buffered buffer to fill */
+static int power_buffer_write_to = 1;
+
+/** Flag indicating buffer is being transmitted */
+static int usb_sending = 0;
 
 static float rtc_freq = 0;
 static uint32_t rtc_loop = 0;
@@ -74,6 +100,7 @@ void RTC_IRQHandler()
         RTC_IntClear(RTC_IFC_OF);
     }
 }
+
 static int UsbDataTransmitted(USB_Status_TypeDef status, uint32_t xferred,
         uint32_t remaining)
 {
@@ -81,9 +108,45 @@ static int UsbDataTransmitted(USB_Status_TypeDef status, uint32_t xferred,
     (void) xferred;
     (void) remaining;
 
-    locked = 0;
+    usb_sending = 0;
+    send_current_buffer();
+
     return USB_STATUS_OK;
 }
+
+static void send_current_buffer()
+{
+    if (usb_sending || power_buffer_sizes[power_buffer_write_to] == 0)
+    {
+        return;
+    }
+
+    usb_sending = 1;
+
+    const char* buf = power_buffers[power_buffer_write_to];
+    int buf_len = power_buffer_sizes[power_buffer_write_to];
+
+    USBD_Write(EP_POWER_IN, (void*) buf, buf_len, UsbDataTransmitted);
+
+    // Clear buffer size and rotate
+    power_buffer_sizes[power_buffer_write_to] = 0;
+    power_buffer_write_to ^= 1;
+}
+
+static float get_timestamp(uint32_t rtc_timestamp)
+{
+    uint32_t now = RTC_CounterGet();
+    uint32_t loops_now = rtc_loop;
+    if (now < rtc_timestamp)
+    {
+        loops_now -= 1;
+    }
+
+    float timestamp = loops_now * (1 << 24) / rtc_freq
+            + (float) rtc_timestamp / rtc_freq;
+    return timestamp;
+}
+
 static void powercon_measure(uint32_t rtc_timestamp, float voltage, float shunt,
         float current)
 {
@@ -92,36 +155,27 @@ static void powercon_measure(uint32_t rtc_timestamp, float voltage, float shunt,
         return;
     }
 
-    uint32_t now = RTC_CounterGet();
-    uint32_t loops_now = rtc_loop;
+    float timestamp = get_timestamp(rtc_timestamp);
 
-    if (now < rtc_timestamp)
+    int allowed_space = BUF_MAX_SIZE
+            - power_buffer_sizes[power_buffer_write_to];
+
+    // Write data to selected buffer
+    int len = snprintf(power_buffers[power_buffer_write_to], allowed_space,
+            "POWER, %f, %f, %f, %f\n", timestamp, voltage, current, shunt);
+
+    if (len >= allowed_space)
     {
-        DEBUG("Reducing loop => %d (ts %u, now %u)\n", loops_now, rtc_timestamp, now);
-        loops_now -= 1;
-    }
-
-    float timestamp = loops_now * (1 << 24) / rtc_freq
-            + (float) rtc_timestamp / rtc_freq;
-
-    if (!locked)
-    {
-        int len = snprintf(power_buffer, 255, "POWER, %f, %f, %f, %f\n", timestamp,
-                voltage, current, shunt);
-
-        if (len > 255)
-        {
-            len = 255;
-        }
-
-        locked = 1;
-        USBD_Write(EP_POWER_IN, (void*) power_buffer, len, UsbDataTransmitted);
+        DEBUG("P Failed %u %u\n", len, allowed_space);
     }
     else
     {
-        DEBUG("Locked\n");
+        power_buffer_sizes[power_buffer_write_to] += len;
+        // try to send
+        send_current_buffer();
     }
 }
+
 void usbpowercon_pinstatechange(uint32_t rtc_timestamp, uint32_t state)
 {
     if (!activated)
@@ -133,35 +187,22 @@ void usbpowercon_pinstatechange(uint32_t rtc_timestamp, uint32_t state)
         return;
     }
 
-    uint32_t now = RTC_CounterGet();
-    uint32_t loops_now = rtc_loop;
+    float timestamp = get_timestamp(rtc_timestamp);
+    int allowed_space = BUF_MAX_SIZE
+            - power_buffer_sizes[power_buffer_write_to];
 
-    if (now < rtc_timestamp)
+    int len = snprintf(power_buffers[power_buffer_write_to], allowed_space,
+            "STATE, %f, %u\n", timestamp, state);
+
+    if (len >= allowed_space)
     {
-        DEBUG("Reducing loop => %d (ts %u, now %u)\n", loops_now, rtc_timestamp, now);
-        loops_now -= 1;
-    }
-
-    float timestamp = loops_now * (1 << 24) / rtc_freq
-            + (float) rtc_timestamp / rtc_freq;
-
-    if (!locked)
-    {
-        int len = snprintf(power_buffer, 255, "STATE, %f, %u\n", timestamp,
-                state);
-
-        if (len > 255)
-        {
-            len = 255;
-        }
-
-        locked = 1;
-        USBD_Write(EP_POWER_IN, (void*) power_buffer, len, UsbDataTransmitted);
-
-        pin_last_state = state;
+        DEBUG("S Failed %u %u\n", len, allowed_space);
     }
     else
     {
-        DEBUG("Locked\n");
+        power_buffer_sizes[power_buffer_write_to] += len;
+        // try to send
+        send_current_buffer();
+        pin_last_state = state;
     }
 }
